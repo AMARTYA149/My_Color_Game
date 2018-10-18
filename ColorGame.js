ab952522dcccf348c13c8c05cdc7da447703c256
Amartya Aishwarya <amartya970914@gmail.com>
2018-10-19 02:52:08 +0530

    part 7 done

--- ColorGame.js
+++ ColorGame.js
@@ -60,7 +60,7 @@ colorDisplay.textContent = pickedColor;
 for(var i=0; i< squares.length; i++){
    squares[i].style.background = colors[i];
 }
-h1.style.background = "#232323";
+h1.style.background = "steelblue";
 })
 
 
